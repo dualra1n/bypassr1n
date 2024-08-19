af0170ba18d7a198967564a91f42c39f3d111bf4
edwin <edwin101004@gmail.com>
2024-08-19 11:25:25 -0400

    update install.sh?

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash/
+#!/bin/bash
 if [ "$EUID" -ne 0 ]
   then
   echo "Please run with SUDO (as root)"
@@ -8,11 +8,11 @@ if [[ -f "/etc/lsb-release" || -f "/etc/debian_version" ]]
     then
         echo "Linux Ubuntu detected and is supported"
     else
-        echo "This Linux Distro is not supported, exiting..."
+        echo "This Linux Distro is not supported for install dependencies, maybe you should install them manually ..."
         exit
     fi
 echo "Written by ios_rifox"
-sudo add-apt-repository universe && sudo apt-get update && sudo apt install libimobiledevice-utils libusbmuxd-tools git curl python3-pip unzip libc6-i386 -y && python3 -m pip install pyimg4 && sudo wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo rm libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo wget https://github.com/darlinghq/darling/releases/download/v0.1.20220704/darling_0.1.20220704.focal_amd64.deb && sudo dpkg -i darling_0.1.20220704.focal_amd64.deb && sudo rm darling_0.1.20220704.focal_amd64.deb
+sudo add-apt-repository universe && sudo apt-get update && sudo apt install libimobiledevice-utils libusbmuxd-tools git curl python3-pip unzip libc6-i386 -y && python3 -m pip install pyimg4 #&& sudo wget http://nz2.archive.ubuntu.com/ubuntu/pool/main/o/openssl/libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo dpkg -i libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo rm libssl1.1_1.1.1f-1ubuntu2.17_amd64.deb && sudo wget https://github.com/darlinghq/darling/releases/download/v0.1.20220704/darling_0.1.20220704.focal_amd64.deb && sudo dpkg -i darling_0.1.20220704.focal_amd64.deb && sudo rm darling_0.1.20220704.focal_amd64.deb
 echo "All Done!"
 echo ""
 echo "[+] Installing linux packages..."
